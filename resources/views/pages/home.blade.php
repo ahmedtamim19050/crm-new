4a8a48e5c6bb90de6b3e2b18487713932e30ccd0
digitalisertweb <73783611+digitalisertweb@users.noreply.github.com>
2023-12-13 23:36:10 +0100

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.front.app')
 @section('content')
    
-    <!--====== SERVICES PART START ======-->
+    <!--====== SERVICES PART1 START ======-->
     
     <section id="why" class="services-area pt-110 pb-120">
         <div class="container">
@@ -57,60 +57,75 @@
                             <p class="text">Our snippet tool make it easy to set up your AI bot in your own website, and get all the data back in to our CRM platform.</p>
                         </div>
                     </div> <!-- single services -->
-					<div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-services services-color-5 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
-                        <div class="services-icon d-flex align-items-center justify-content-center">
-                            <i class="lni lni-layers"></i>
-                        </div>
-                        <div class="services-content">
-                            <h4 class="services-title"><a href="#">CRM</a></h4>
-                            <p class="text">Controll all your leads what a intuitiv solution that convert them into clients.</p>
-                        </div>
-                    </div> <!-- single services -->
-                </div>
-				<div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-services services-color-6 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
-                        <div class="services-icon d-flex align-items-center justify-content-center">
-                            <i class="lni lni-layers"></i>
-                        </div>
-                        <div class="services-content">
-                            <h4 class="services-title"><a href="#">CRM</a></h4>
-                            <p class="text">Controll all your leads what a intuitiv solution that convert them into clients.</p>
-                        </div>
-                    </div> <!-- single services -->
-                </div>
-				<div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-services services-color-7 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
-                        <div class="services-icon d-flex align-items-center justify-content-center">
-                            <i class="lni lni-layers"></i>
-                        </div>
-                        <div class="services-content">
-                            <h4 class="services-title"><a href="#">CRM</a></h4>
-                            <p class="text">Controll all your leads what a intuitiv solution that convert them into clients.</p>
-                        </div>
-                    </div> <!-- single services -->
-                </div>
-				<div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="single-services services-color-8 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
-                        <div class="services-icon d-flex align-items-center justify-content-center">
-                            <i class="lni lni-layers"></i>
-                        </div>
-                        <div class="services-content">
-                            <h4 class="services-title"><a href="#">CRM</a></h4>
-                            <p class="text">Controll all your leads what a intuitiv solution that convert them into clients.</p>
-                        </div>
-                    </div> <!-- single services -->
-                </div>
                 </div>
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
     
-    <!--====== SERVICES PART ENDS ======-->
+    <!--====== SERVICES PART1 ENDS ======-->
+	
+	<!--====== SERVICES PART2 START ======-->
+    
+    <section id="why" class="services-area pt-110 pb-120">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-7">
+                    <div class="section-title text-center pb-25">
+                        <h3 class="title">Why You Should Choose AppLand</h3>
+                        <p class="text">Alii nusquam cu duo, vim eu consulatu percipitur, meis dolor comprehensam at vis. Vel ut percipitur dignissim signiferumque.</p>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-services services-color-1 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.2s">
+                        <div class="services-icon d-flex align-items-center justify-content-center">
+                            <i class="lni lni-layers"></i>
+                        </div>
+                        <div class="services-content">
+                            <h4 class="services-title"><a href="#">CRM</a></h4>
+                            <p class="text">Controll all your leads what a intuitiv solution that convert them into clients.</p>
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-services services-color-2 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.4s">
+                        <div class="services-icon d-flex align-items-center justify-content-center">
+                            <i class="lni lni-layout"></i>
+                        </div>
+                        <div class="services-content">
+                            <h4 class="services-title"><a href="#">SoMe</a></h4>
+                            <p class="text">Schedule all social media content and target each post against your leads spesific needs.</p>
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-services services-color-3 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.6s">
+                        <div class="services-icon d-flex align-items-center justify-content-center">
+                            <i class="lni lni-bolt"></i>
+                        </div>
+                        <div class="services-content">
+                            <h4 class="services-title"><a href="#">Newsletter</a></h4>
+                            <p class="text">Target your new leads with the automation tool.</p>
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-services services-color-4 text-center mt-30 wow fadeInUpBig" data-wow-duration="1.3s" data-wow-delay="0.8s">
+                        <div class="services-icon d-flex align-items-center justify-content-center">
+                            <i class="lni lni-protection"></i>
+                        </div>
+                        <div class="services-content">
+                            <h4 class="services-title"><a href="#">AI Bot</a></h4>
+                            <p class="text">Our snippet tool make it easy to set up your AI bot in your own website, and get all the data back in to our CRM platform.</p>
+                        </div>
+                    </div> <!-- single services -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    
+    <!--====== SERVICES PART2 ENDS ======-->
     
     <!--====== ABOUT PART START ======-->
     
