71b0935ed9f869959252284fc33a7c587056b8f6
digitalisertweb <73783611+digitalisertweb@users.noreply.github.com>
2023-12-14 01:14:01 +0100

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -157,9 +157,10 @@
                         <h3 class="download-title">What are you waiting for?</h3>
                         <p class="text">Test our free version that are full specked of functions. <b>No credit card required</b>.</p>
                         <ul>
-                            <li><a class="app-store" href="#"><img src="{{asset('frontend-assets/images/app-store.png')}}" alt="store"></a></li>
-                            <li><a class="play-store" href="#"><img src="{{asset('frontend-assets/images/play-store.png')}}" alt="store"></a></li>
-							<a href="/login" rel="nofollow" class="main-btn">Get started for free!</a>
+                            <li> <p class="text">Test our free version that are full specked of functions. <b>No credit card required</b>.</p></li>
+                        </ul>
+						<ul>
+                            <a href="/login" rel="nofollow" class="main-btn">Get started for free!</a>
                         </ul>
                     </div> <!-- download image -->
                 </div>
