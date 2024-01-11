df3ef6aef0c45700bbfee11b45b1a14187445720
Ahmed Tamim <ahmedtamim19050@gmail.com>
2024-01-11 16:40:34 +0600

    issue

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -76,7 +76,7 @@ class ClientController extends Controller
         
         $client=Client::find($id);
         $persons=Person::where('user_id',auth()->id())->pluck('last_name','id')->toArray();
-        $organisations=Organisation::where('user_id',auth()->id())->pluck('name','id')->toArray();
+        $organisations=Organisation::where('user_created_id ',auth()->id())->pluck('name','id')->toArray();
         return view('dashboard.clients.show', [
             'client' => $client,
             'persons' => $persons,
